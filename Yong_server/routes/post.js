652ab13ea259f4e94b752ec4dfdf89a437880dba
LeeYongwuk <138418818+LeeYongwuk@users.noreply.github.com>
2023-12-05 15:23:29 +0900

    스케마

--- Yong_server/routes/post.js
+++ Yong_server/routes/post.js
@@ -19,17 +19,17 @@ const Post = mongoose.model('Post', postSchema);
 
 // 게시물 저장 POST 요청 처리
 app.post('/post', async (req, res) => {
-    const { title, body, author, location } = req.body;
+    const { title, body, author, location } = req.body; // location : 게시물 작성 위치 (클라이언트에서 전송)
 
     try {
         console.log('게시물 저장 요청 확인함');
 
         // 게시물 생성
         const newPost = new Post({
-            title,
-            body,
-            author,
-            location,
+            title: title, // 제목
+            body: body, // 본문
+            writer: author, // 작성자 아이디
+            location: location, // 게시물 작성 위치
         });
 
         // 데이터베이스에 저장
